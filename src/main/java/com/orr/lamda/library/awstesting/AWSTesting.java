0a2ae1beef445fc0d45a09a6a1dd0069d18e811f
MK Patil <patil.mk@gmail.com>
2019-03-19 15:27:55 +0530

    added files

---
+++ src/main/java/com/orr/lamda/library/awstesting/AWSTesting.java
@@ -0,0 +1,9 @@
+package com.orr.lamda.library.awstesting;
+
+public class AWSTesting {
+
+    public static void main(String[] args) throws Exception {
+
+        System.out.println("testing 123");
+    }
+}
